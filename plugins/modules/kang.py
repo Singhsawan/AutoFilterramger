d7f9ae4257536fcb50bd4e443315b3ca4dcc9c8b
Akhand Bharat <125857863+Singhsawan@users.noreply.github.com>
2024-04-23 19:20:53 +0530

    Update kang.py

--- plugins/modules/kang.py
+++ plugins/modules/kang.py
@@ -30,9 +30,6 @@ def getConfig(name: str):
 
 try:
     BOT_TOKEN = getConfig('BOT_TOKEN')
-except KeyError as e:
-    LOGGER.error("BOT_TOKEN env variables missing! Exiting now")
-    exit(1)
 
 updater = telegram.ext.Updater(token=BOT_TOKEN)
 bot = updater.bot
