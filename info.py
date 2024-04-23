acbae01b7ab5cbc1f72075babb79c8522e0c4544
Akhand Bharat <125857863+Singhsawan@users.noreply.github.com>
2024-04-23 17:27:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -150,8 +150,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://{ragnardiscussion-9260f30a3b61}/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://{ragnardiscussion-9260f30a3b61}/".format(FQDN, PORT)
+URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://{}/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -166,9 +166,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',True))
 if HAS_SSL:
-    URL = "https://{ragnardiscussion-9260f30a3b61}/".format(FQDN)
+    URL = "https://{}/".format(FQDN)
 else:
-    URL = "http://{ragnardiscussion-9260f30a3b61}/".format(FQDN)
+    URL = "http://{}/".format(FQDN)
 
 # add premium logs channel id
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002108139365'))
